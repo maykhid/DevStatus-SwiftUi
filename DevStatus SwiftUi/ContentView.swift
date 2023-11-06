4281518906bf0accce9541e078a6068e2fff8ba2
Henry Ifebunandu <henryifebunandu@gmail.com>
2023-11-06 09:35:30 +0100

    complete devstatus swift ui

--- DevStatus SwiftUi/ContentView.swift
+++ DevStatus SwiftUi/ContentView.swift
@@ -8,16 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    let backgroundColor = UIColor(hex: "#395266")
+    @State private var username: String = ""
+    @State private var navigateToNextScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Color(uiColor: backgroundColor).ignoresSafeArea()
+            
+            VStack(alignment: .leading){
+                Text("Developer Status").font(.title).fontWeight(.bold).foregroundColor(Color.white)
+                
+                Spacer()
+                    .frame(height: 60.0)
+                
+                VStack{
+                    Text("Check").font(.headline).fontWeight(.bold).foregroundColor(Color.white)
+                }
+                
+                Spacer()
+                    .frame(height: 30.0)
+                
+                TextField(
+                        "Enter GitHub Username ",
+                        text: $username
+                    ).textFieldStyle(CustomTextFieldStyle())
+                    .background(Color.white)
+                
+                Spacer()
+                    .frame(height: 25.0)
+                
+                Button {
+                    navigateToNextScreen = true
+                } label: {
+                    Text("Check Status").font(.subheadline).fontWeight(.semibold).foregroundColor(Color(uiColor: backgroundColor))
+                }
+                .frame(height: 45.0)
+                .buttonStyle(.plain)
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                
+                
+            }
+            .padding(.horizontal, 25.0)
+        }.navigate(to: DevInfo(), when: $navigateToNextScreen)
+        
+        
+    }
+        
+}
+
+struct CustomTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<_Label>) -> some View {
+        configuration
+            .padding(10)
+            .background(RoundedRectangle(cornerRadius: 0).stroke(Color.white, lineWidth: 2))
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ DevStatus SwiftUi/ViewNavigator.swift
@@ -0,0 +1,34 @@
+//
+//  ViewNavigator.swift
+//  DevStatus SwiftUi
+//
+//  Created by Henry Ifebunandu on 11/4/23.
+//
+
+import SwiftUI
+
+extension View {
+    /// Navigate to a new view.
+    /// - Parameters:
+    ///   - view: View to navigate to.
+    ///   - binding: Only navigates when this condition is `true`.
+    func navigate<NewView: View>(to view: NewView, when binding: Binding<Bool>) -> some View {
+        NavigationView {
+            ZStack {
+                self
+                    .navigationBarTitle("")
+                    .navigationBarHidden(false)
+
+                NavigationLink(
+                    destination: view
+                        .navigationBarTitle("")
+                        .navigationBarHidden(false),
+                    isActive: binding
+                ) {
+                    EmptyView()
+                }
+            }
+        }
+        .navigationViewStyle(.stack)
+    }
+}
